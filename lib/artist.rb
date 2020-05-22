b78397198690f4b5449d6f091b8325de37ac5a82
Kamrin Kennedy <kamrinkennedy@gmail.com>
2020-05-22 13:40:51 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -29,3 +29,4 @@ class Artist
   end
   
 end
+
--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -13,9 +13,7 @@ class MP3Importer
   end
 
   def import 
-    files.each do |file|
-      Song.new_by_filename(file)
-    end
+    files.each {|file| Song.new_by_filename(file)}
   end 
   
 end
